14ba3e0277c070fa860e679938cf4ca0402517bb
Deepak Paliwal <deepakpaliwal16@gmail.com>
2026-03-07 21:59:59 +0530

    updated redis.js

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,11 +1,9 @@
 const Redis = require('ioredis');
 const env = require('./env');
 
-const redis = new Redis({
-  host: env.redis.host,
-  port: env.redis.port,
-  lazyConnect: true,
-});
+const redis = process.env.REDIS_URL
+  ? new Redis(process.env.REDIS_URL, { lazyConnect: true })
+  : new Redis({ host: env.redis.host, port: env.redis.port, lazyConnect: true });
 
 redis.on('connect', () => console.log('Redis connected'));
 redis.on('error', (err) => console.error('Redis error:', err.message));
