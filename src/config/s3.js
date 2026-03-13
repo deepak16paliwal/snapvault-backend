6975526990007dfb4c1dac9e926e72908d6635c3
Deepak Paliwal <deepakpaliwal16@gmail.com>
2026-03-13 08:55:59 +0530

    s3 changes to upload data

--- src/config/s3.js
+++ src/config/s3.js
@@ -7,6 +7,11 @@ const s3 = new S3Client({
     accessKeyId: env.aws.accessKeyId,
     secretAccessKey: env.aws.secretAccessKey,
   },
+  // Disable automatic checksum injection on presigned PUT URLs.
+  // Without this, SDK v3 adds x-amz-checksum-crc32 to presigned URLs,
+  // which browsers cannot satisfy since they don't know the checksum before upload.
+  requestChecksumCalculation: 'WHEN_REQUIRED',
+  responseChecksumValidation: 'WHEN_REQUIRED',
 });
 
 module.exports = s3;
